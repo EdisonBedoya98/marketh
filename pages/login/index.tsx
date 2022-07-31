accd5a4302936ff0bf00ebe4d26e643f21e5e95e
edison bedoya <edison.bedoya@micontrato.co>
2022-07-31 14:31:35 -0500

    update

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,72 +1,72 @@
-import React, { useState } from "react";
-import { auth } from "../../src/store/firebase-config";
-import { useRouter } from "next/router";
+// import React, { useState } from "react";
+// import { auth } from "../../src/store/firebase-config";
+// import { useRouter } from "next/router";
 
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-import styles from "./login.module.scss";
+// import {
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword,
+// } from "firebase/auth";
+// import styles from "./login.module.scss";
 
-const Login = () => {
-  const [login, setLogin] = useState(true);
+// const Login = () => {
+//   const [login, setLogin] = useState(true);
 
-  const router = useRouter();
+//   const router = useRouter();
 
-  const handleSubmit = async (event) => {
-    // Prevent page reload
-    event.preventDefault();
-    var { user, pass } = document.forms[0];
-    if (login) {
-      try {
-        const session = await signInWithEmailAndPassword(
-          auth,
-          user.value,
-          pass.value
-        );
-        router.push("/home");
-      } catch (e) {
-        console.log(e);
-        const tag = document.getElementById("errorMessage");
-        tag.style.display = "block";
-      }
-    } else {
-      try {
-        const account = await createUserWithEmailAndPassword(
-          auth,
-          user.value,
-          pass.value
-        );
-        console.log("creado");
-      } catch (e) {
-        console.log(e);
-      }
-    }
-  };
-  return (
-    <div className={styles.login}>
-      <h1>MARKETH </h1>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <div>
-          <label>Celular o Correo</label>
-          <input type="text" name="user" required />
-        </div>
-        <div>
-          <label>Contraseña</label>
-          <input type="password" name="pass" required />
-        </div>
-        <span id="errorMessage" className={styles.messageError}>
-          Error del celular, correo o contraseña
-        </span>
-        <button type="submit">
-          {login ? "Iniciar sesión" : "Registrarse"}
-        </button>
-      </form>
-      <button onClick={() => setLogin(!login)}>
-        {login ? "Crear cuenta" : "Volver a iniciar sesión"}
-      </button>
-    </div>
-  );
-};
+//   const handleSubmit = async (event) => {
+//     // Prevent page reload
+//     event.preventDefault();
+//     var { user, pass } = document.forms[0];
+//     if (login) {
+//       try {
+//         const session = await signInWithEmailAndPassword(
+//           auth,
+//           user.value,
+//           pass.value
+//         );
+//         router.push("/home");
+//       } catch (e) {
+//         console.log(e);
+//         const tag = document.getElementById("errorMessage");
+//         tag.style.display = "block";
+//       }
+//     } else {
+//       try {
+//         const account = await createUserWithEmailAndPassword(
+//           auth,
+//           user.value,
+//           pass.value
+//         );
+//         console.log("creado");
+//       } catch (e) {
+//         console.log(e);
+//       }
+//     }
+//   };
+//   return (
+//     <div className={styles.login}>
+//       <h1>MARKETH </h1>
+//       <form onSubmit={handleSubmit} className={styles.form}>
+//         <div>
+//           <label>Celular o Correo</label>
+//           <input type="text" name="user" required />
+//         </div>
+//         <div>
+//           <label>Contraseña</label>
+//           <input type="password" name="pass" required />
+//         </div>
+//         <span id="errorMessage" className={styles.messageError}>
+//           Error del celular, correo o contraseña
+//         </span>
+//         <button type="submit">
+//           {login ? "Iniciar sesión" : "Registrarse"}
+//         </button>
+//       </form>
+//       <button onClick={() => setLogin(!login)}>
+//         {login ? "Crear cuenta" : "Volver a iniciar sesión"}
+//       </button>
+//     </div>
+//   );
+// };
 
-export default Login;
+// export default Login;
