251ef07f4fe2043d1ba9612ab7f9f05aa664bee0
Sebastian Ruiz <imsebarz@gmail.com>
2022-07-31 13:50:16 -0500

    updated shop page

--- src/components/shop/index.tsx
+++ src/components/shop/index.tsx
@@ -3,7 +3,33 @@ import styles from './shop.module.scss'
 
 const Shop = () => {
   return (
-    <div className={styles.shop}>Shop</div>
+    <div className={styles.shop}>
+      <h1>Hello, User!</h1>
+      <div className={styles.searchbar}>Search</div>
+      <h2>Categories</h2>
+      <div className={styles.categories}>
+        <div className={`${styles.categorie} ${styles.fincas}`}>
+          <h1>
+            Fincas
+          </h1>
+        </div>
+        <div className={`${styles.categorie} ${styles.productos}`}>
+          <h1>
+            Productos
+          </h1>
+        </div>
+        <div className={`${styles.categorie} ${styles.ofertas}`}>
+          <h1>
+            Ofertas
+          </h1>
+        </div>
+        <div className={`${styles.categorie} ${styles.rapido}`}>
+          <h1>
+            Rápido
+          </h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
